23059850dd85c70d8de6c91e7faceea8f82fb841
Brandon Fan <brandonfan1256@gmail.com>
2020-03-13 11:03:12 -0400

    update augment_data script

--- scripts/augment_data.sh
+++ scripts/augment_data.sh
@@ -4,3 +4,4 @@ python ./augment.py --input=../data/news_data/titles.txt --output=../data/augmen
 python ./augment.py --input=../data/news_data/titles.txt --output=../data/augmented_news_data/titles_augmented_0.2.txt --num_aug=16 --alpha=0.2
 python ./augment.py --input=../data/news_data/titles.txt --output=../data/augmented_news_data/titles_augmented_0.1.txt --num_aug=16 --alpha=0.1
 python ./augment.py --input=../data/news_data/titles.txt --output=../data/augmented_news_data/titles_augmented_0.05.txt --num_aug=16 --alpha=0.05
+python ./create_augmented_dataset.py
